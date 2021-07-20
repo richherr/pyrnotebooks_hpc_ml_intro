aed616c63babc6337524329d9faeba5a43212fec
Richard Herrington <richherr@neptune.acs.unt.edu>
2021-07-20 01:52:34 -0500

    .

--- binder/install.R
+++ binder/install.R
@@ -1,8 +1,5 @@
 install.packages("Rcpp", repos="https://cran.microsoft.com/")
-require(unixtools)
-devtools::install_github("sachsmc/ggkm")
-devtools::install_github("benjaminrich/table1")
-devtools::install_github("slowkow/ggrepel")
+install.packages("remotes", repos="https://cran.microsoft.com/")
 install.packages("tinytex", repos="https://cran.microsoft.com/")
 tinytex::install_tinytex()
 
@@ -71,7 +68,6 @@ system(paste("rm -rf ", path, sep=""))
 system("mkdir /tmp/new_tmp")
 set.tempdir("/tmp/new_tmp")
 
-install.packages("remotes", repos="https://cran.microsoft.com/")
 install.packages("shiny", repos="https://cran.microsoft.com/")
 install.packages("knitr", repos="https://cran.microsoft.com/")
 
